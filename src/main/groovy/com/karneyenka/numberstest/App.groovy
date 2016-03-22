b7cf7b58b547004c2761c7a772b62b9f320b5a59
Andrei Karneyenka <eyforia@gmail.com>
2016-03-22 12:25:50 -0400

    reorganized laucher

--- src/main/groovy/com/karneyenka/numberstest/App.groovy
+++ src/main/groovy/com/karneyenka/numberstest/App.groovy
@@ -5,13 +5,22 @@ import static groovyx.gpars.GParsExecutorsPool.withPool
 public class App {
 
     public static void main(String[] args) {
+        assert args.length == 2, "Usage: java -jar numbers.jar <Input File> <Dictionary File>\n" +
+                "e.g. java -jar input.txt dictionary.txt\n"
+
+        def inputFileName = args[0]
+        def dictionaryFileName = args[1]
+
         long time = System.currentTimeMillis()
-        Dictionary dictionary = new Dictionary(new FileInputStream("data/dictionary.txt"))
+        println "Loading dictionary from $dictionaryFileName..."
+        Dictionary dictionary = new Dictionary(new FileInputStream(dictionaryFileName))
         PhraseBuilder phraseBuilder = new PhraseBuilder(dictionary)
         PhoneNumberFactory factory = new PhoneNumberFactory(phraseBuilder)
 
-        withPool(Runtime.getRuntime().availableProcessors()) {  //split between cores for faster processing
-            new FileInputStream("data/input.txt")
+        int numCores = Runtime.getRuntime().availableProcessors()
+        println "Processing on $numCores cores ..."
+        withPool(numCores) {  //split between cores for faster processing
+            new FileInputStream(inputFileName)
                     .readLines()//iterate lines
                     .collectParallel { String line -> factory.create(line) } //convert lines to PhoneNumbers
                     .findAll { PhoneNumber number -> number.phrases }  //find numbers with has phrases
